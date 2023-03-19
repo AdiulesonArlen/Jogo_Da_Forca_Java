baef7d0134e69daf146541d3e2aa209c3d54a957
Adiuleson Arlen <adiulesonarlen.ads@gmail.com>
2023-03-18 21:18:08 -0300

    Java.util

--- src/JogoDaForca.java
+++ src/JogoDaForca.java
@@ -1,6 +1,14 @@
-import java.util.Scanner
+import java.util.Scanner;
 public class JogoDaForca {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
 
+        
+
+
+
+
+
+        input.close();
     }
 }
